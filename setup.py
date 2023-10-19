e19993268e4c0591b55ccf73cfeb58c497787de1
ajCameron <alexanderjfcameron@gmail.com>
2023-10-19 04:04:25 +0100

    Copying in example, fixing setup.py, updating requirements.txt
     - copying in the examples from the main mewbot proeject
     - updating requirements to include needed mewbot modules and the feedreader requirement
     - updating setup.py to actually work

--- setup.py
+++ setup.py
@@ -129,17 +129,7 @@ setuptools.setup(
         'Programming Language :: Python :: Implementation :: PyPy',
         'Operating System :: OS Independent',
 
-{ % if cookiecutter.license == "MIT" - %}
         'License :: OSI Approved :: MIT License',
-{ % - elif cookiecutter.license == "BSD-3" - %}
-        'License :: OSI Approved :: BSD License',
-{ % - elif cookiecutter.license == "GNU GPL v3.0" - %}
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-{ % - elif cookiecutter.license == "Apache Software License 2.0" - %}
-        'License :: OSI Approved :: Apache Software License',
-{ % - elif cookiecutter.license == "Mozilla Public License 2.0" - %}
-        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-{ % - endif %}
 
     ],
 
